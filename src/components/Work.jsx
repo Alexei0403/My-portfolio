9e4e6e7b301a785454831bff219b9608f152d00e
Alexei0403 <maryinkself@gmail.com>
2024-07-22 06:38:11 +0300

    this is my portfolio

---
+++ src/components/Work.jsx
@@ -0,0 +1,37 @@
+/** @format */
+
+import { useTranslation } from "react-i18next";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+
+const Work = ({ workImage, workTitle, workLink }) => {
+  const { i18n, t } = useTranslation();
+
+  return (
+    <div className="column is-6">
+      <div className="card portfolio-item">
+        <div className="card-header">
+          <figure className="image">
+            <img src={workImage} alt="portfolio" />
+          </figure>
+        </div>
+        <div className="card-footer">
+          <div className="is-flex">
+            <h3>{workTitle}</h3>
+            <div className="preview">
+              <a href={workLink} target="_blank">
+                <span>{t("project.preview")}</span>
+                {i18n.language === "ar" ? (
+                  <FaChevronLeft />
+                ) : (
+                  <FaChevronRight />
+                )}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
---
+++ src/data/skills.jsx
@@ -0,0 +1,40 @@
+/** @format */
+
+// On-page SEO
+// Technical SEO
+// Off-Page SEO
+// Content Marketing
+// SEO Audits
+// Shopify SEO
+// Wordpress SEO
+// Ecommerce SEO and much more!
+
+import OnPageSeo from "../assets/images/skills/on-page.png";
+import TechicalSeo from "../assets/images/skills/technical.png";
+import OffPageSeo from "../assets/images/skills/off-page_yellow.png";
+import AuditSeo from "../assets/images/skills/audit_cube.png";
+import ContentSeo from "../assets/images/skills/content_col.png";
+import ECommerceSeo from "../assets/images/skills/ecommerce_green.png";
+import MuchMore from "../assets/images/skills/much.png";
+import ShopifySeo from "../assets/images/skills/shopify.png";
+import WordPressSeo from "../assets/images/skills/wordpress.png";
+import BackLinks from "../assets/images/skills/backlinks.png";
+import WebDevelope from "../assets/images/skills/webdevelope.png";
+import SEOStrategy from "../assets/images/skills/strategy.png";
+
+const skillsData = [
+  { title: "SeoStrategy", icon: SEOStrategy },
+  { title: "OnpageSeo", icon: OnPageSeo },
+  { title: "TechnicalSeo", icon: TechicalSeo },
+  { title: "OffpageSeo", icon: OffPageSeo },
+  { title: "contentMarket", icon: ContentSeo },
+  { title: "Audits", icon: AuditSeo },
+  { title: "ShopifySEO", icon: ShopifySeo },
+  { title: "BackLinksSEO", icon: BackLinks },
+  { title: "WordpressSEO", icon: WordPressSeo },
+  { title: "EcommerceSEO", icon: ECommerceSeo },
+  { title: "WebDevelope", icon: WebDevelope },
+  { title: "Muchmore", icon: MuchMore },
+];
+
+export default skillsData;
